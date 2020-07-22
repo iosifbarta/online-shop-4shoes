50ceb4e10b9479bd525ec3801e57e30ccdd16f05
Iosif.Barta <iosif.barta@gmail.com>
2020-07-22 16:08:46 +0300

    Initial commit

---
+++ src/main/java/org/fastrackit/onlineshop4shoes/OnlineShop4shoesApplication.java
@@ -0,0 +1,13 @@
+package org.fastrackit.onlineshop4shoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShop4shoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShop4shoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fastrackit/onlineshop4shoes/OnlineShop4shoesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fastrackit.onlineshop4shoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShop4shoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
