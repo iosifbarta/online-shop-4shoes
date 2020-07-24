6f9a60b7fa85cfe960006887e01ceef72540cc50
Iosif.Barta <iosif.barta@gmail.com>
2020-07-24 17:02:32 +0300

    added methods get, update and delete products, added test for them

--- src/main/java/org/fastrackit/onlineshop4shoes/service/ProductService.java
+++ src/main/java/org/fastrackit/onlineshop4shoes/service/ProductService.java
@@ -1,12 +1,16 @@
 package org.fastrackit.onlineshop4shoes.service;
 
 import org.fastrackit.onlineshop4shoes.domain.Product;
+import org.fastrackit.onlineshop4shoes.exception.ResourceNotFoundException;
 import org.fastrackit.onlineshop4shoes.persistence.ProductRepository;
 import org.fastrackit.onlineshop4shoes.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -34,5 +38,29 @@ public class ProductService {
         product.setImageUrl(request.getImageUrl());
 
         return productRepository.save(product);
+    }
+
+
+    public Product getProduct(long id){
+
+        LOGGER.info("Retrieving product {}", id);
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product " + id + " not found."));
+    }
+
+    public Product updateProduct(long id, SaveProductRequest request){
+
+        LOGGER.info("Updating product {}: {} ", id, request );
+
+        Product product = getProduct(id);
+
+        BeanUtils.copyProperties(request, product);
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(long id){
+
+        LOGGER.info("Deleting product {} ", id);
+        productRepository.deleteById(id);
     }
 }
--- src/test/java/org/fastrackit/onlineshop4shoes/ProductServiceIntegrationTests.java
+++ src/test/java/org/fastrackit/onlineshop4shoes/ProductServiceIntegrationTests.java
@@ -1,8 +1,10 @@
 package org.fastrackit.onlineshop4shoes;
 
 import org.fastrackit.onlineshop4shoes.domain.Product;
+import org.fastrackit.onlineshop4shoes.exception.ResourceNotFoundException;
 import org.fastrackit.onlineshop4shoes.service.ProductService;
 import org.fastrackit.onlineshop4shoes.transfer.SaveProductRequest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,25 +28,7 @@ class ProductServiceIntegrationTests {
     @Test
     void createProduct_whenValidRequest_thenReturnCreatedProduct() {
 
-        SaveProductRequest request = new SaveProductRequest();
-        request.setBrandName("Epica");
-        request.setPrice(149);
-        request.setGender("F");
-        request.setSize(37);
-        request.setShoeCode("HM207-302 05-N");
-        request.setQuantity(4);
-
-
-        Product product = productService.createProduct(request);
-
-        assertThat(product, notNullValue());
-        assertThat(product.getId(), greaterThan(0L));
-        assertThat(product.getBrandName(), is(request.getBrandName()));
-        assertThat(product.getPrice(), is(request.getPrice()));
-        assertThat(product.getSize(), is(request.getSize()));
-        assertThat(product.getGender(), is(request.getGender()));
-        assertThat(product.getShoeCode(), is(request.getShoeCode()));
-        assertThat(product.getQuantity(), is(request.getQuantity()));
+        createProduct();
     }
 
     @Test
@@ -136,5 +120,117 @@ class ProductServiceIntegrationTests {
         } catch (Exception e) {
             assertThat("Unexpected exception thrown", e instanceof ConstraintViolationException);
         }
+    }
+
+    @Test
+    void getProduct_whenExistingProduct_theReturnProduct(){
+
+        Product product = createProduct();
+
+        Product response = productService.getProduct(product.getId());
+
+        assertThat(response, notNullValue());
+        assertThat(response.getId(), is(product.getId()));
+        assertThat(response.getBrandName(), is(product.getBrandName()));
+        assertThat(response.getShoeCode(), is(product.getShoeCode()));
+        assertThat(response.getPrice(), is(product.getPrice()));
+        assertThat(response.getQuantity(), is(product.getQuantity()));
+        assertThat(response.getSize(), is(product.getSize()));
+        assertThat(response.getGender(), is(product.getGender()));
+        assertThat(response.getImageUrl(), is(product.getImageUrl()));
+        assertThat(response.getDescription(), is(product.getDescription()));
+
+    }
+    @Test
+    void getProduct_whenNonExistingProduct_thenThrowResourceNotFoundException(){
+
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> productService.getProduct(0));
+    }
+
+    @Test
+    void updateProduct_whenValidRequest_thenReturnUpdateProduct (){
+
+        Product product = createProduct();
+
+        SaveProductRequest request = new SaveProductRequest();
+
+        request.setBrandName(product.getBrandName()+ " Updated");
+        request.setShoeCode(product.getShoeCode()+ " Updated");
+        request.setQuantity(product.getQuantity()+ 5);
+        request.setPrice(product.getPrice()+ 1);
+
+
+        Product updateProduct = productService.updateProduct(product.getId(), request);
+
+        assertThat(updateProduct, notNullValue());
+        assertThat(updateProduct.getId(), is(product.getId()));
+        assertThat(updateProduct.getBrandName(), is(request.getBrandName()));
+        assertThat(updateProduct.getShoeCode(), is(request.getShoeCode()));
+        assertThat(updateProduct.getQuantity(), is(request.getQuantity()));
+        assertThat(updateProduct.getPrice(), is(request.getPrice()));
+    }
+
+    @Test
+    void updateProduct_whenNonExistingProduct_thenThrowResourceNotFoundException(){
+
+        SaveProductRequest request = new SaveProductRequest();
+
+        request.setBrandName("Updated");
+        request.setShoeCode(" Updated");
+        request.setQuantity(5);
+        request.setPrice(1);
+
+        try {
+            Product updateProduct = productService.updateProduct(20, request);
+
+            assertThat(updateProduct, notNullValue());
+            assertThat(updateProduct.getId(), is(20));
+            assertThat(updateProduct.getBrandName(), is(request.getBrandName()));
+            assertThat(updateProduct.getShoeCode(), is(request.getShoeCode()));
+            assertThat(updateProduct.getQuantity(), is(request.getQuantity()));
+            assertThat(updateProduct.getPrice(), is(request.getPrice()));
+        }catch (Exception e) {
+            assertThat("Unexpected exception thrown", e instanceof ResourceNotFoundException);
+
+        }
+
+    }
+
+    @Test
+    void deleteProduct_whenExistingProduct_thenProductDoesNotExistAnymore(){
+
+
+        Product product = createProduct();
+        productService.deleteProduct(product.getId());
+
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> productService.getProduct(product.getId()));
+
+
+    }
+
+
+    private Product createProduct() {
+        SaveProductRequest request = new SaveProductRequest();
+        request.setBrandName("Gabor");
+        request.setPrice(219);
+        request.setGender("F");
+        request.setSize(36);
+        request.setShoeCode("GB72510 05-N");
+        request.setQuantity(7);
+
+        Product product = productService.createProduct(request);
+
+        assertThat(product, notNullValue());
+        assertThat(product.getId(), greaterThan(0L));
+        assertThat(product.getBrandName(), is(request.getBrandName()));
+        assertThat(product.getPrice(), is(request.getPrice()));
+        assertThat(product.getSize(), is(request.getSize()));
+        assertThat(product.getGender(), is(request.getGender()));
+        assertThat(product.getShoeCode(), is(request.getShoeCode()));
+        assertThat(product.getQuantity(), is(request.getQuantity()));
+
+        return product;
     }
 }
